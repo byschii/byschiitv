231a3863432649847883bacf6b6e2ea0dd88b15a
byschii <byschii@gmail.com>
2025-10-10 18:29:33 +0200

    aggiunsto script per caricare tp1

---
+++ carica_twin_peaks.sh
@@ -0,0 +1,17 @@
+
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E01.Pilot.v2.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E02.Traces.To.Nowhere.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E03.Zen.Or.The.Skill.To.Catch.A.Kill.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E04.Rest.In.Pain.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E05.The.One.Armed.Man.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E06.Coopers.Dreams.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E07.Realization.Time.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+curl 'http://localhost:8080/enque//media/7.%20serie/twin%20peaks/Twin.Peaks.S01/Twin.Peaks.S01E08.The.Last.Evening.720p.Bluray.AC3.ITA.DTS.ENG.Subs.x264-HDitaly.mkv'
+
+curl http://localhost:8080/start
+
+
+
+
+
+
