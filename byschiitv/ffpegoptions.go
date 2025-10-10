79253dee3997220273fba62cb574dab0048c3e98
byschii <byschii@gmail.com>
2025-10-10 18:48:40 +0200

    fixato comando ffmpeg?

--- byschiitv/ffpegoptions.go
+++ byschiitv/ffpegoptions.go
@@ -22,16 +22,13 @@ func FfmpegCommand(videoPath string, rtmpURL string) []string {
 func FfmpegLightCommand(videoPath string, rtmpURL string) []string {
 	// Example: ffmpeg -re -i input.mp4 -c copy -f flv rtmp://localhost/live/stream
 	sliceCommand := []string{
-		"-re",           // read at native frame rate
-		"-i", videoPath, // input file
-		"-vf", "scale=1280:720", // scale video to 720p
+		"-re",                  // read at native frame rate
 		"-c:v", "h264_v4l2m2m", // video codec
-		"-preset", "veryfast", // encoding speed
-		"-tune", "zerolatency", // low latency
-		"-c:a", "aac", // audio codec
-		"-b:a", "96k", // audio bitrate
-		"-b:v", "2000k", // video bitrate
-		"-f", "flv", // output format
+		"-b:v", "1000k", "-maxrate", "1200k", "-bufsize 2000k", // video bitrate
+		"-vf", "scale=854:480:flags=fast_bilinear,fps=25,format=yuv420p", // scale video to 480p
+		"-g", "50", "-keyint_min", "50", // GOP size
+		"-num_output_buffers", "32", "-num_capture_buffers", "16",
+		"-c:a", "aac", "-b:a", "64k", "-ar", "44100", "-ac", "2", // audio codec
 		rtmpURL}
 
 	return sliceCommand
