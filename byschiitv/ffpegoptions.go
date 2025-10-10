082fac0c9c39109171434807feb269a9997ba8b3
byschii <byschii@gmail.com>
2025-10-10 18:53:12 +0200

    fix ffmpeg?

--- byschiitv/ffpegoptions.go
+++ byschiitv/ffpegoptions.go
@@ -1,22 +1,5 @@
 package main
 
-func FfmpegCommand(videoPath string, rtmpURL string) []string {
-	// Example: ffmpeg -re -i input.mp4 -c copy -f flv rtmp://localhost/live/stream
-	sliceCommand := []string{
-		"-re",           // read at native frame rate
-		"-i", videoPath, // input file
-		"-c:v", "h264_v4l2m2m", // video codec
-		"-preset", "veryfast", // encoding speed
-		"-tune", "zerolatency", // low latency
-		"-c:a", "aac", // audio codec
-		"-b:a", "128k", // audio bitrate
-		"-f", "flv", // output format
-		rtmpURL,
-	}
-
-	return sliceCommand
-}
-
 // streamToRTMP starts an FFmpeg command to stream a video file to nginx-rtmp.
 // It listens on ctx and stops the stream when cancelled.
 func FfmpegLightCommand(videoPath string, rtmpURL string) []string {
@@ -24,11 +7,18 @@ func FfmpegLightCommand(videoPath string, rtmpURL string) []string {
 	sliceCommand := []string{
 		"-re",                  // read at native frame rate
 		"-c:v", "h264_v4l2m2m", // video codec
-		"-b:v", "1000k", "-maxrate", "1200k", "-bufsize 2000k", // video bitrate
-		"-vf", "scale=854:480:flags=fast_bilinear,fps=25,format=yuv420p", // scale video to 480p
-		"-g", "50", "-keyint_min", "50", // GOP size
-		"-num_output_buffers", "32", "-num_capture_buffers", "16",
-		"-c:a", "aac", "-b:a", "64k", "-ar", "44100", "-ac", "2", // audio codec
+		"-b:v", "1000k",
+		"-maxrate", "1200k",
+		"-bufsize", "2000k", // video bitrate
+		"-vf", "scale=1280,720:flags=fast_bilinear,fps=30,format=yuv420p", // scale video to 480p
+		"-g", "50",
+		"-keyint_min", "50", // GOP size
+		"-num_output_buffers", "32",
+		"-num_capture_buffers", "16",
+		"-c:a", "aac",
+		"-b:a", "64k",
+		"-ar", "44100",
+		"-ac", "2", // audio codec
 		rtmpURL}
 
 	return sliceCommand
